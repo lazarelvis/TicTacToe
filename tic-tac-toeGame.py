880869cf8c1321b0619c1968f786b0089b247506
Lazar <U4312@in-tech.global>
2022-04-12 11:27:04 +0300

    tic tac toe game using python

--- tic-tac-toeGame.py
+++ tic-tac-toeGame.py
@@ -6,6 +6,8 @@ board = [
     ["_", "_", "_"]
 ]
 
+game_over = False 
+
 def display_board(board):
     for row in board:
         print(row)
@@ -24,19 +26,63 @@ def convert_selection(selection):
     selection -= 1
     return (selection // 3, selection % 3)
 
-def place_piece(selection, board):
-    board[selection[0]][selection[1]] = 'X'
+# verify if is not ocupied and place x or 0
+def place_piece(selection,is_x, board):
+        if board[selection[0]][selection[1]] == '_':
+            print("O trun") if is_x else print("X turn") 
+            board[selection[0]][selection[1]] = "X" if is_x else "O"
+        else:
+            raise ValueError
 
-# error handling if is not a number & place X to a square
+# error handling if is not a number & place piece to a square
 def main():
-    while True:
+    is_x = True
+    game_over = False
+    print("X trun") 
+    while not game_over:
         display_board(board)
         try:
             selection = convert_selection(select_square())
-            place_piece(selection, board)
+            place_piece(selection, is_x, board)
         except ValueError:
-            print("It's not a number")
+            print("It's not a number or is ocupied")
+            continue
+        game_over = is_win(board) or is_draw(board)
+        is_x = not is_x
 
-main 
+# the board is full
+def is_draw(board):
+    for row in board:
+        for val in row:
+            if val == "_":
+                return False
+    display_board(board)
+    print("Draw!")
+    return True
+
+# winer 8 posib
+def is_win(board):
+    winner = None
+    for i in range(3):
+        # horizontal
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != "_":
+            winner = board[i][0]
+            break
+        # vertical
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] != "_":
+            winner = board[0][i]
+            break
+    # diagonal
+    if board[1][1] != "_":
+        if (board[0][0] == board[1][1] == board[2][2]
+            or board[0][2] == board[1][1] == board[2][0]):
+            winner = board[1][1]
+    if winner is not None:
+        display_board(board)
+        print(f"{winner} is the winner!")
+        return True
+    return False
+
+# main
 if __name__ == "__main__":
     main()
